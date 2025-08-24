32da3928004d5a5167f4b3f3ce8ed71766d68a73
Busra2456 <you@example.com>
2025-08-24 18:38:11 +0600

    Dashboard server

---
+++ index.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.POST || 5000;
+//middleware
+app.use(cors());
+app.use(express.json())
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.whg4qi0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in 
+    // 4.7)
+    await client.connect();
+
+ const menuCollection = client.db("bossDb").collection("menu");
+ const reviewsCollection = client.db("bossDb").collection("reviews")
+ const cartsCollection = client.db("bossDb").collection("carts")
+
+      app.get('/menu',async(req,res) =>{
+            const result = await menuCollection.find().toArray();
+            res.send(result)
+      })
+       app.get('/reviews',async(req,res) =>{
+            const result = await reviewsCollection.find().toArray();
+            res.send(result)
+      })
+      //cards collection
+      app.get('/carts',async(req,res)=>{
+        const email = req.query.email;
+        const query ={email:email};
+        const result = await cartsCollection.find(query).toArray();
+        res.send(result);
+
+      })
+      app.post('/carts', async(req,res)=>{
+        const cartItem = req.body;
+        const result = await cartsCollection.insertOne(cartItem);
+        res.send(result);
+
+      })
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+//     await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req,res)=>{
+      res.send('boss is sitting')
+})
+app.listen(port,() =>{
+      console.log(`Bistro boss is sitting on port ${port}`)
+})
